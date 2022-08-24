f5dece913040fea1a7af12a9f96b6ee5f8a481b9
Diana <diana@MacBook-Pro-Diana.local>
2022-08-25 01:27:31 +0300

    task1 was added in HW2

---
+++ HW2/task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int temp1 = number / 10;
+int result = temp1 % 10;
+
+Console.WriteLine("Вторая цифра числа " + number + ": " + result);
