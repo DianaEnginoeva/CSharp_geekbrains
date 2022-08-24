e93d95bb755f458af56489c289fd40fe3db80000
Diana <diana@MacBook-Pro-Diana.local>
2022-08-25 01:58:51 +0300

    HW2 is done

---
+++ HW2/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number / 100 > 0) {
+	if (number / 1000 > 0) {
+		while (number / 1000 > 0) {
+			number = number / 10;
+		}
+	}
+	int result = number % 100 % 10;
+	Console.WriteLine("Третья цифра числа: " + result);
+}
+else {
+	Console.WriteLine("У числа нет третьей цифры");
+}
+
---
+++ HW2/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7) {
+	Console.WriteLine("Это выходной день");
+}
+else if (number >= 1 && number < 7) {
+	Console.WriteLine("Это рабочий день");
+}
+
+
