30b3c28f7fe0e30d7995049c37497a2a3cced6c1
Diana <diana@MacBook-Pro-Diana.local>
2022-08-25 22:22:30 +0300

    Seminar 3 was created

---
+++ Seminar3/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка
+Console.WriteLine("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x>0 && y>0) {
+	Console.WriteLine("Точка располагается в 1 четверти плоскости");
+}
+else if (x>0 && y<0) {
+	Console.WriteLine("Точка располагается в 4 четверти плоскости");
+	}
+else if (x<0 && y>0) {
+	Console.WriteLine("Точка располагается в 2 четверти плоскости");
+}
+else if (x<0 && y<0) {
+	Console.WriteLine("Точка располагается в 3 четверти плоскости");
+}
+else if (x==0 || y==0) {
+	Console.WriteLine("Невозможно определить");
+}
---
+++ Seminar3/task2/Program.cs
@@ -0,0 +1,58 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
+// Console.WriteLine("Введите номер четверти: ");
+// int part = Convert.ToInt32(Console.ReadLine());
+
+// if (part == 1) {
+// 	Console.WriteLine("x = [0; +бесконечность]; y = [0; +бесконечность]");
+// }
+// else if (part == 2) {
+// 	Console.WriteLine("x = [0; -бесконечность]; y = [0; +бесконечность]");
+// }
+// else if (part == 3) {
+// 	Console.WriteLine("x = [0; -бесконечность]; y = [0; -бесконечность]");
+// }
+// else if (part == 4) {
+// 	Console.WriteLine("x = [0; +бесконечность]; y = [0; -бесконечность]");
+// }
+// else {
+// 	Console.WriteLine("Введено неверное значение");
+// }
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+// Console.WriteLine("Введите x первой точки: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите y первой точки: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите x второй точки: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите y второй точки: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = Math.Pow(x2-x1, 2);
+// double y = Math.Pow(y2-y1, 2);
+
+// double result = Math.Sqrt(x + y);
+
+// // то же самое в одну строку
+// // double result = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+
+// Console.WriteLine(result);
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// while (count<=number) {
+// 	double result = Math.Pow(count, 2);
+// 	Console.WriteLine(result);
+// 	count++;
+// }
+
+for (int count = 1; count<=number; count++) {
+	double result = Math.Pow(count, 2);
+	Console.WriteLine(result);
+}
