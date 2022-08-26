edca7e7b2abfa95e0a65f2aaad4755d8ce06d3a5
Diana <diana@MacBook-Pro-Diana.local>
2022-08-26 16:13:37 +0300

    HW3 is done

---
+++ HW3/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+int length = number.Length;
+string number2 = "";
+
+for (int i=length-1; i>=0; i--) {
+	 number2 += number[i];
+}
+if (number==number2) {
+	Console.WriteLine("Число " + number + " является палиндромом");
+}
+else {
+	Console.WriteLine("Число " + number + " не является палиндромом");
+}
