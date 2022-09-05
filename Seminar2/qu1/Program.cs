7df93a732a389c258e66970f64ef87a29d16f688
Diana <diana@MacBook-Pro-Diana.local>
2022-09-06 01:50:11 +0300

    section links 1

--- Seminar2/qu1/Program.cs
+++ Seminar2/qu1/Program.cs
@@ -3,14 +3,15 @@ string day = "Понедельник";
 Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number == 1) day = "Понедельник";
-if (number == 2) day = "Вторник";
-if (number == 3) day = "Среда";
-if (number == 4) day = "Четверг";
-if (number == 5) day = "Пятница";
-if (number == 6) day = "Суббота";
-if (number == 7) day = "Воскресенье";
-if (number < 1 || number > 7) day = "Число введено неверно";
+else if (number == 2) day = "Вторник";
+else if (number == 3) day = "Среда";
+else if (number == 4) day = "Четверг";
+else if (number == 5) day = "Пятница";
+else if (number == 6) day = "Суббота";
+else if (number == 7) day = "Воскресенье";
+else day = "Число введено неверно";
+// if (number < 1 || number > 7) day = "Число введено неверно";
 // else { day = "Число введено неверно"; }
-// почему это не работает?
+// почему это не работает? Потому что у каждого условия должно было быть "else if", иначе твое else относится только к последнему условию (==7)
 
 Console.WriteLine(day);
