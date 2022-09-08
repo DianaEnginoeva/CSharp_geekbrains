8efdd8f6c868bfa43a408c8c13e30fcc42f43588
Diana <diana@MacBook-Pro-Diana.local>
2022-09-08 21:04:26 +0300

    Some improvements for task1

--- HW6/task1/Program.cs
+++ HW6/task1/Program.cs
@@ -2,13 +2,13 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Введите длину массива: ");
+Console.WriteLine("Введите количество чисел: ");
 int length = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[length];
 int positiveNumbersAmount = 0;
 for (int i=0; i<length; i++)
 {
-	Console.WriteLine("Введите значение элемента массива: ");
+	Console.WriteLine("Введите число " + (i+1) + ": ");
 	array[i]=Convert.ToInt32(Console.ReadLine());
 	if (array[i]>0) positiveNumbersAmount++;
 }
