8ffbd23710c73a26f286dc552075c48aee3074be
Diana <diana@MacBook-Pro-Diana.local>
2022-09-08 21:00:55 +0300

    HW6 is done

---
+++ HW6/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+int positiveNumbersAmount = 0;
+for (int i=0; i<length; i++)
+{
+	Console.WriteLine("Введите значение элемента массива: ");
+	array[i]=Convert.ToInt32(Console.ReadLine());
+	if (array[i]>0) positiveNumbersAmount++;
+}
+
+Console.WriteLine("Количество чисел больше нуля: " + positiveNumbersAmount);
