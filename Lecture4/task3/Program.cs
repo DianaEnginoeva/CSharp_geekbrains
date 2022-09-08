f0ced83d5ae3fb3a407cce73677431233ac48b81
Diana <diana@MacBook-Pro-Diana.local>
2022-09-08 16:35:14 +0300

    Lecture 4 is done

---
+++ Lecture4/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// вычисление факториала
+
+// int Factorial(int n) {
+// 	if(n==1) return 1;
+// 	else return n*Factorial(n-1);
+// } 
+
+// Console.WriteLine(Factorial(5)); //все работает хорошо
+// Console.WriteLine(Factorial(17)); // получаем отрицательное число, потому что тип int переполнен; чтобы этого не происходило - воспользуемся типом double в качестве возвращаемого ниже
+
+// вычисление факториала
+
+double Factorial(int n) {
+	if(n==1) return 1;
+	else return n*Factorial(n-1);
+} 
+
+Console.WriteLine(Factorial(5)); //все работает хорошо
+Console.WriteLine(Factorial(17)); //все окей
