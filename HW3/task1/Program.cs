095a03774a5c72e1e0f3e4afa2037f2e40aed332
Diana <diana@MacBook-Pro-Diana.local>
2022-09-11 00:18:40 +0300

    I fixed task1 (Warning CS8602: Разыменование вероятной пустой ссылки)

--- HW3/task1/Program.cs
+++ HW3/task1/Program.cs
@@ -1,15 +1,20 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
 Console.WriteLine("Введите число: ");
 string? number = Console.ReadLine();
-int length = number.Length;
-string number2 = "";
 
-for (int i=length-1; i>=0; i--) {
-	 number2 += number[i];
+if (number!=null) {
+	int length = number.Length;
+ 	string number2 = "";
+
+	for (int i=length-1; i>=0; i--) {
+		number2 += number[i];
+	}
+	if (number==number2) {
+		Console.WriteLine("Число " + number + " является палиндромом");
+	}
+	else {
+		Console.WriteLine("Число " + number + " не является палиндромом");
+	}
 }
-if (number==number2) {
-	Console.WriteLine("Число " + number + " является палиндромом");
-}
-else {
-	Console.WriteLine("Число " + number + " не является палиндромом");
-}
+
+
