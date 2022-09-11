be9695a0b4f2e42ae527e60c6b20ba05590c1e4b
Diana <diana@MacBook-Pro-Diana.local>
2022-09-12 01:39:09 +0300

    added condition in task2

--- HW6/task2/Program.cs
+++ HW6/task2/Program.cs
@@ -10,9 +10,8 @@ double b2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 
-if (b1==b2 && k1==k2) {
-	Console.WriteLine("Прямые совпадают");
-}
+if (b1==b2 && k1==k2) Console.WriteLine("Прямые совпадают");
+else if (k1 == k2) Console.WriteLine("Прямые параллельны");
 else {
 	double x = ((b2-b1)/(k1-k2));
 
