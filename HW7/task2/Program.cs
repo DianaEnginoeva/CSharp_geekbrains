cecba21037d3d08648292c13e9ec8caeacba47f8
Diana <diana@MacBook-Pro-Diana.local>
2022-09-12 20:09:48 +0300

    HW7 was fixed

--- HW7/task2/Program.cs
+++ HW7/task2/Program.cs
@@ -5,12 +5,26 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-int[,] array = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+// тестовый массив
+// int[,] array = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+
+int[,] array = new int[new Random().Next(1, 10), new Random().Next(1, 10)];
+
+for(int i=0; i<array.GetLength(0); i++)
+{
+	for(int j=0; j<array.GetLength(1); j++)
+	{
+		array[i, j] = new Random().Next(0, 10);
+		Console.Write(array[i, j] + "\t");
+		
+	}
+	Console.WriteLine();
+}
 
 Console.WriteLine("Введите номер строки: ");
 int row = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите номер столбца: ");
 int column = Convert.ToInt32(Console.ReadLine());
 
-if(row>array.GetLength(0) || column>array.GetLength(1)) Console.WriteLine("Такого элемента не существует.");
+if(row>=array.GetLength(0) || column>=array.GetLength(1)) Console.WriteLine("Такого элемента не существует.");
 else Console.WriteLine(array[row, column]);
