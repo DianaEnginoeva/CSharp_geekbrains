74010fcf3a0edd03be559179eb14b7be83ea8c0c
Diana <diana@MacBook-Pro-Diana.local>
2022-09-22 03:55:47 +0300

    HW9 is done

---
+++ HW9/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int Numbers(int m, int n)
+{
+	if (m>n) return 0;
+	else return m + Numbers(m+1, n);
+}
+
+Console.WriteLine(Numbers(numberM, numberN));
---
+++ HW9/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int functionAckermann(int m, int n)
+{
+	if (m == 0) return (n + 1);
+	if (n == 0) return functionAckermann(m - 1, 1);
+	return functionAckermann(m - 1, functionAckermann(m, n - 1));
+}
+
+Console.WriteLine(functionAckermann(numberM, numberN));
+
+
